a33ced82cdded4289961f2541ef09cbb9cb81f17
sarita595k <sarita595k@gmail.com>
2025-09-03 17:36:22 +0530

    new api fetch for movies list

--- Netflix_Frontend/src/Components/DashboardMoviesList.jsx
+++ Netflix_Frontend/src/Components/DashboardMoviesList.jsx
@@ -13,7 +13,7 @@ export const DashboardMoviesList = () => {
 
     const fetchMovies = async () => {
         try {
-            const response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=426a13d01b97cad474a6729816a0e719&source_id=203&watch_region=IN&sort_by=primary_release_date.lte=2025-09-03")
+            const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=426a13d01b97cad474a6729816a0e719&source_id=203&watch_region=IN&sort_by=primary_release_date.lte=${Date.now()}`)
             const data = await response.json();
             setMoviesList(data.results)
             console.log(data);
