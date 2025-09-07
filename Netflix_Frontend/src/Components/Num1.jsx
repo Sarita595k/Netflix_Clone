7d821e707e03424200cf717a966949d6896b7af4
sarita595k <sarita595k@gmail.com>
2025-09-07 20:13:32 +0530

    main header for dashboard created

---
+++ Netflix_Frontend/src/Components/Num1.jsx
@@ -0,0 +1,7 @@
+export const Num1 = () => {
+    return (
+        <>
+            <h1>Number 1 show/movie</h1>
+        </>
+    )
+}
--- Netflix_Frontend/src/Pages/Dashboard.jsx
+++ Netflix_Frontend/src/Pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import { MoviesApi } from "../Components/MoviesApi"
+import { Num1 } from "../Components/Num1"
 import { ShowsApi } from "../Components/ShowsApi"
 
 export const Dashboard = () => {
     return (
         <>
             <h1>Welcome to your dashboard</h1>
+            <Num1 />
             <MoviesApi />
             <ShowsApi />
         </>
