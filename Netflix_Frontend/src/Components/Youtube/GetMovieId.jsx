5064e8914c81cea86d287a42b6189463aad3ce3a
sarita595k <sarita595k@gmail.com>
2025-09-13 21:20:59 +0530

    small change

--- Netflix_Frontend/src/Components/Youtube/GetMovieId.jsx
+++ Netflix_Frontend/src/Components/Youtube/GetMovieId.jsx
@@ -4,6 +4,7 @@ import { GetYouTubeUrl } from "./GetYoutubeUrl";
 export const GetMovieId = (props) => {
     return (
         <>
+            {/* to get the video url  */}
             <GetYouTubeUrl id={props.idIs} type="tv" />
         </>
     )
